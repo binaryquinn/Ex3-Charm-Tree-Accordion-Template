087358d4147cf6a94ebea3af72dcbe1895f9f82c
binaryquinn <bquinn@ics.com>
2017-09-27 16:30:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,40 +6,6 @@ window.onload=function(){
 }
 google.script.run.withSuccessHandler(makeAccordion).makeAll();
 
-mxTooltipHandler.prototype.ignoreTouchEvents = !1;
-
-mxTooltipHandler.prototype.reset = function(a, b) {
-  var c = a.getState(),
-    d = a.getSource(),
-    e = a.getX(),
-    f = a.getY(),
-    g = c != null ? a.isSource(c.shape) || a.isSource(c.text) : !1;
-  var a = this.graph.getTooltip(c, d, e, f);
-
-  if (!this.destroyed && null != a && 0 < a.length) {
-    null === this.div && this.init();
-    $("#modalContent").html(a.replace(/\n/g, "\x3cbr\x3e"));
-    var headings = c.text.value.split("<br>");
-    $("#charmName").text(headings[0].slice(3, -4));
-    if (headings.length > 2) {
-      $("#charmAbility").text(headings[1]);
-      $("#charmEssence").text(headings[2]);
-
-    } else {
-      $("#charmAbility").text("");
-      $("#charmEssence").text("");
-
-    }
-
-
-  }
-
-  this.state = c;
-  this.node = d;
-  this.stateSource = g;
-
-};
-
 $(document).on("mousedown touch","g[style], g[transform]", function(event) {
   modal.style.display = "block";
   var maximum = (($(window).height()) / 2) - ($(".modal-header").height() + $(".modal-footer").height());
